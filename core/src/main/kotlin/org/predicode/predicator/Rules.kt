fb752fc4871bd92622d790c508a65664a4348de0
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2018-05-18 15:30:17 +0700

    Implements scanning rule selector

---
+++ core/src/main/kotlin/org/predicode/predicator/Rules.kt
@@ -0,0 +1,42 @@
+@file:JvmName("Rules")
+package org.predicode.predicator
+
+import reactor.core.publisher.Mono
+import reactor.core.publisher.toFlux
+import java.util.*
+
+/**
+ * Creates scanning predicate resolution rule selector.
+ *
+ * It scans the given [rules] for the matches.
+ *
+ * @param rules rules to scan.
+ */
+fun ruleMatcher(vararg rules: Rule): Rule.Selector = SequentialRuleSelector(rules)
+
+private class SequentialRuleSelector(private val rules: Array<out Rule>) : Rule.Selector {
+
+    override fun ruleMatches(pattern: RulePattern, knowns: Knowns) =
+            rules.toFlux().flatMap { rule ->
+                Mono.justOrEmpty(rule.condition.match(pattern, knowns)?.let { updatedKnowns ->
+                    Rule.Match(rule, updatedKnowns)
+                })
+            }
+
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as SequentialRuleSelector
+
+        return Arrays.equals(rules, other.rules)
+    }
+
+    override fun hashCode() = Arrays.hashCode(rules)
+
+    override fun toString(): String {
+        return "{\n  ${rules.joinToString("\n  ")}\n}"
+    }
+
+}
