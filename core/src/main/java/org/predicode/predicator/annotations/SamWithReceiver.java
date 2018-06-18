14108b8f80ffe2b8e0a4f2583b1184bcc1adc577
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2018-06-18 13:58:45 +0700

    Proper retention

--- core/src/main/java/org/predicode/predicator/annotations/SamWithReceiver.java
+++ core/src/main/java/org/predicode/predicator/annotations/SamWithReceiver.java
@@ -1,4 +1,9 @@
 package org.predicode.predicator.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+
+@Target(ElementType.TYPE)
 public @interface SamWithReceiver {
 }
