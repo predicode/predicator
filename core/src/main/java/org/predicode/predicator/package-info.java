cbb45ebedefbbe0cb837b906e022a6bb0bc32d12
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2018-07-10 12:18:17 +0700

    Add package-info files

---
+++ core/src/main/java/org/predicode/predicator/package-info.java
@@ -0,0 +1,4 @@
+@ParametersAreNonnullByDefault
+package org.predicode.predicator;
+
+import javax.annotation.ParametersAreNonnullByDefault;
---
+++ core/src/main/java/org/predicode/predicator/terms/package-info.java
@@ -0,0 +1,4 @@
+@ParametersAreNonnullByDefault
+package org.predicode.predicator.terms;
+
+import javax.annotation.ParametersAreNonnullByDefault;
